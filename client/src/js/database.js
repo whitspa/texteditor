d54b4b2d769fbdfb5c0c3b1c5cce5b0b384d74c9
whit <whitspa64@gmail.com>
2023-03-19 13:24:51 -0400

    added database GET and PUT logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,37 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+//Added logic that accepts some content and adds it to the database
+ 
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to database');
+//creates connection to db
+  const contactDb = await openDB('jate', 1);
+//creates new transaction and specifies database and version
+  const trans = contactDb.transaction('jate,readwrite');
+//creates new transaction and specifies database and privileges
+  const store = trans.objectStore('jate');
+//designate the desired objectStore to open
+  const request = store.put({ id: 1, value: content});
+//logic to pass content into the store
+  const result = await request;
+  console.log("Data saved to the database", result);
+};
+
+
+
+// Added logic that gets all the content from the database
+export const getDb = async () => {
+console.log("GET from database");
+const contactDb = await openDB('jate', 1);
+const trans = contactDb.transaction('jate', 'readonly');
+const store = trans.objectStore('jate');
+const request = store.getAll();
+
+const result = await request;
+console.log('result.value', result);
+return result?.value;
+};
 
 initdb();
