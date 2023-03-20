d415e58cccb1938482837a208de6993355611ef9
whit <whitspa64@gmail.com>
2023-03-20 10:48:11 -0400

    implemented asset caching in the rehgister route function

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,18 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+//Implemented asset caching in the registerRoute function
+registerRoute(
+  ({request}) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevaalidate({cacheName:'asset-cache',
+plugins: [
+  new CacheableResponsePlugin({
+    statuses: [0, 200],
+  }),
+
+  new ExpirationPlugin({
+    maxEntries: 60,
+    maxAgeSeconds: 30 * 24 * 60 * 60,
+  })
+]})
+);
